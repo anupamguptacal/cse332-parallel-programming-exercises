11f302138f787e09563d0001c7b5cc5c9d3bbe2e
anupamguptacal <anupam@d-69-91-132-87.dhcp4.washington.edu>
2017-02-10 11:19:52 -0800

    Pushing hw

--- src/hasOver/HasOver.java
+++ src/hasOver/HasOver.java
@@ -5,8 +5,55 @@ import java.util.concurrent.RecursiveTask;
 import cse332.exceptions.NotYetImplementedException;
 
 public class HasOver {
+	private static int CUTOFF;
+	private static ForkJoinPool POOL = new ForkJoinPool();
     public static boolean hasOver(int val, int[] arr, int sequentialCutoff) {
-        throw new NotYetImplementedException();
+    	CUTOFF = sequentialCutoff;
+    	return POOL.invoke(new MaxTask(val, arr, 0, arr.length));
+    }
+    
+    static class MaxTask extends RecursiveTask<Boolean> {
+    	int[] arr;
+    	int value;
+    	int low;
+    	int hi;
+    	public MaxTask(int val, int[] arrayParse, int lo, int high) {
+    		this.arr = arrayParse;
+    		this.value = val;
+    		this.low = lo;
+    		this.hi = high;
+    	}
+
+		@Override
+		protected Boolean compute() {
+			if(hi - low <= CUTOFF) {
+    			if(returnMax(arr, low, hi) > value) {
+    				return true;
+    			} else {
+    				return false;
+    			}
+    		}
+    		int mid = low + (hi - low)/2;
+    		MaxTask left = new MaxTask(value, arr, low, mid);
+    		MaxTask right = new MaxTask(value, arr, mid, hi);
+    		
+    		right.fork();
+    		
+    		boolean leftgetAnswer = left.compute();
+    		boolean rightAnswer = right.join();
+    		
+    		return leftgetAnswer || rightAnswer;
+		}
+    	
+    }
+    private static int returnMax(int[]arr, int lo, int hi) {
+    	int max = arr[lo];
+    	for(int i = lo + 1; i < hi; i++) {
+    		if(arr[i] > max) {
+    			max = arr[i];
+    		}
+    	}
+    	return max;
     }
 
     private static void usage() {
--- src/longestSequence/LongestSequence.java
+++ src/longestSequence/LongestSequence.java
@@ -4,11 +4,94 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 import cse332.exceptions.NotYetImplementedException;
 
+
 public class LongestSequence {
+	private static int CUTOFF;
+	private static ForkJoinPool POOL = new ForkJoinPool();
     public static int getLongestSequence(int val, int[] arr, int sequentialCutoff) {
-        throw new NotYetImplementedException();
+      CUTOFF = sequentialCutoff;
+     SequenceRange result =  POOL.invoke(new LargestSeq(val, arr, 0, arr.length));
+     System.out.println("Answer = " + result.sequenceLength);
+     return result.longestRange;
     }
 
+    static class LargestSeq extends RecursiveTask<SequenceRange> {
+    	int arr[];
+    	int val;
+    	int low;
+    	int high;
+		@Override
+		protected SequenceRange compute() {
+			if(high - low <= CUTOFF) {				
+				SequenceRange max = returnMaxSequence(val, arr, low, high);
+				int maximumValue = Math.max(max.longestRange, Math.max(max.matchingOnLeft, max.matchingOnRight));			
+				return new SequenceRange(max.matchingOnLeft, max.matchingOnRight, maximumValue,Math.min(high, arr.length) - low);			
+			}
+			int mid = low + (high - low)/2;
+			
+			LargestSeq left = new LargestSeq(val, arr, low, mid);
+			LargestSeq right = new LargestSeq(val, arr, mid, high);
+			
+			right.fork();
+			
+			SequenceRange leftValue = left.compute();
+			SequenceRange rightValue = right.join();
+			
+			// finding maximum
+			int leftValueTreaty = leftValue.matchingOnLeft;
+			int rightValueTreaty = rightValue.matchingOnRight;
+			if(leftValue.matchingOnLeft == leftValue.sequenceLength) {
+				leftValueTreaty = leftValue.sequenceLength + rightValue.matchingOnLeft;
+			}
+			if(rightValue.matchingOnRight == rightValue.sequenceLength) {
+				 rightValueTreaty = rightValue.sequenceLength + leftValue.matchingOnRight;
+			}
+			int biggestValue = Math.max(leftValue.longestRange, Math.max(leftValueTreaty, rightValueTreaty));
+			biggestValue = Math.max(biggestValue,  Math.max(rightValue.longestRange, leftValue.matchingOnRight + rightValue.matchingOnLeft));
+			return new SequenceRange(leftValueTreaty, rightValueTreaty, biggestValue, rightValue.sequenceLength + leftValue.sequenceLength);
+		}
+    	public LargestSeq(int value, int[] array, int low, int high) {
+    		this.arr = array;
+    		this.val = value;
+    		this.low = low;
+    		this.high = high;
+
+    	}
+    }
+    public static SequenceRange returnMaxSequence(int val, int[] arr, int lo, int hi) {
+    	int count = 0;
+    	int maxCount = 0;
+    	for(int i = lo; i < Math.min(hi, arr.length); i++) {
+    		if(arr[i] == val) {
+    			count++;
+    		} else {
+    			if(count > maxCount) {
+    				maxCount = count;
+    			}
+    			count = 0;
+    		}
+    	}
+    	
+    	int left = 0;
+    	for(int i = lo; i < Math.min(hi, arr.length); i ++) {
+    		if(arr[i] != val) {
+    			break;
+    		} else {
+    			left++;
+    		}
+    	}
+    	
+    	int right = 0;
+    	for(int i = Math.min(hi - 1, arr.length); i >= lo; i--) {
+    		if(arr[i] != val) {
+    			break;
+    		} else {
+    			right++;
+    		}
+    	}
+    	return new SequenceRange(left, right, Math.max(maxCount, count), Math.min(hi, arr.length) - lo);
+    	
+    	}
     private static void usage() {
         System.err.println("USAGE: LongestSequence <number> <array> <sequential cutoff>");
         System.exit(2);
@@ -31,6 +114,7 @@ public class LongestSequence {
             }
             System.out.println(getLongestSequence(val, arr, Integer.parseInt(args[2])));
         } catch (NumberFormatException e) {
+        	System.err.println(e.getMessage());
             usage();
         }
     }
